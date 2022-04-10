55fb726296cb2dfbdaeda3aba2bf240975f2bad7
IamMahfooz <92675550+IamMahfooz@users.noreply.github.com>
2022-04-10 13:56:51 +0530

    Update aes.sh

--- aes.sh
+++ aes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for file in /home/mahfooz/bash/test-folder/*
 do
-	 openssl enc -aes-256-cbc -pass pass:password -in "$file" -out "$file".enc  
+	 openssl enc -aes-256-cbc -pbkdf2 -pass pass:password -in "$file" -out "$file".enc  
                 rm "$file"
 	done
